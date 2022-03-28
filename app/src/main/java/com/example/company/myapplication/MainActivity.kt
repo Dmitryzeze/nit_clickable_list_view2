2cdf1ca70df3fb9b8bd6837bc0d022e5336d40c6
Sator <lbvf1197@mail.ru>
2022-03-28 13:41:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/company/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.company.myapplication
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val dataArray = MutableList(5) { x -> "Item$x" }
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataArray)
+
+    }
+}
